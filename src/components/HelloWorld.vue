19d24b0563bee28612633733b2f4fd89aa7f9b62
Kyle Bishop <filethirteen@live.com>
2021-02-28 01:56:55 -0700

    Initial commit, working adding and clearing tasks

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,202 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="border container mt-4 bg-secondary">
+    <!-- Modal -->
+    <div class="modal fade" id="addTaskModal" tabindex="-1" aria-labelledby="addTaskModalLabel" aria-hidden="true">
+      <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content bg-dark">
+          <div class="modal-header bg-secondary">
+            <h5 class="modal-title text-light" id="addTaskModalLabel">Add Task</h5>
+            <button id="modalCloseButton" type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+            <div class="form-floating mb-3">
+              <input type="text" class="form-control" id="floatingTaskName" v-model="task.data.attributes.taskName" placeholder="Task Name">
+              <label for="floatingTaskName">Task Name</label>
+            </div>
+            <div class="form-floating">
+              <input type="text" class="form-control" id="floatingTaskDescription" v-model="task.data.attributes.taskDescription" placeholder="Task Description">
+              <label for="floatingTaskDescription">Task Description</label>
+            </div>
+          </div>
+          <div class="modal-footer bg-secondary">
+            <button type="button" class="btn btn-outline-light" data-bs-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-dark" @click="createTask">Save changes</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <h1 class="text-light">Tasks</h1>
+    <div v-if="tasks.length === 0" class="text-light">Click Add Task to start adding tasks</div>
+    <div v-for="(task, index) in tasks" :key="task.attributes.taskName+index" class="row">
+        <div class="col-8 border bg-dark p-2 m-2">
+            <div class="form-check form-check-inline">
+              <input class="form-check-input" type="checkbox" :id="'complete-id-'+index" value="completed">
+              <label class="form-check-label text-light h5" :for="'complete-id-'+index">{{task.attributes.taskName}}</label>
+            </div>
+            <div class="row ps-4 text-light">{{task.attributes.taskDescription}}</div>
+        </div>
+    </div>
+    <div class="row mb-2 mt-2">
+      <button type="button" class="btn btn-dark col-auto ms-2 me-4" data-bs-toggle="modal" data-bs-target="#addTaskModal">Add Task</button>
+      <button type="button" class="btn btn-dark col-auto" @click="deleteAllTasks">Clear All</button>
+    </div>
   </div>
 </template>
 
 <script>
+
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+  mounted() {
+    //this.fetchTasks();
+    this.$nextTick(() => {
+      this.fetchTasks();
+      document.querySelectorAll('.form-check-input').forEach(item => {
+        item.addEventListener('click', () => {
+          console.log("Checkbox clicked");
+        });
+      });
+    });
+  },
+  data() {
+    return {
+      task: {
+        data: {
+          type: "task",
+          attributes: {
+            taskName: "",
+            taskDescription: "",
+            category: "",
+            complete: false,
+            status: ""
+          }
+        }
+      },
+      tasks: [
+          {
+            id: "31dddb5e155b4fd58cacf7a298417ed7",
+            type: "task",
+            links: {
+                self: "http://localhost:8080/tasks/31dddb5e155b4fd58cacf7a298417ed7"
+            },
+            attributes: {
+                taskName: "MyTask",
+                taskDescription: "My task desc",
+                category: "MyCategory",
+                complete: false,
+                status: "To Do"
+            }
+          },
+          {
+            id: "31dddb5e155b4fd58cacf7a298417ed7",
+            type: "task",
+            links: {
+                self: "http://localhost:8080/tasks/31dddb5e155b4fd58cacf7a298417ed7"
+            },
+            attributes: {
+                taskName: "Other Task",
+                taskDescription: "BBBBBBBBBBBBBB",
+                category: "MyCategoryB",
+                complete: false,
+                status: "Doing"
+            }
+          },
+      ]
+    }
+  },
+  methods: {
+    fetchTasks(){
+        const baseURI = 'http://localhost:7000/tasks';
+        this.$http.get(baseURI)
+        .then((result) => {
+          this.tasks = result.data.data;
+        });
+    },
+    patchTask(task){
+      const baseURI = 'http://localhost:7000/tasks/'+task.id;
+      this.$http.patch(baseURI)
+      .then((result) => {
+        console.log(result.status);
+        this.fetchTasks();
+      });
+    },
+    deleteTask(task){
+      const baseURI = 'http://localhost:7000/tasks/'+task.id;
+      this.$http.delete(baseURI)
+      .then((result) => {
+        console.log(result.status);
+        this.fetchTasks();
+      });
+    },
+    deleteAllTasks(){
+      const baseURI = 'http://localhost:7000/deleteAll';
+      this.$http.delete(baseURI)
+      .then((result) => {
+        console.log(result.status);
+        this.fetchTasks();
+      });  
+    },
+    resetTaskModel(){
+      this.task = {
+        data: {
+          type: "task",
+          attributes: {
+            taskName: "",
+            taskDescription: "",
+            category: "",
+            complete: false,
+            status: ""
+          }
+        }
+      };
+    },
+    postTask(task){
+      console.log(task);
+      const baseURI = 'http://localhost:7000/tasks/';
+      const options = {
+        headers: {
+          'Content-Type':'application/vnd.api+json'
+        }
+      };
+      this.$http.post(baseURI, task, options)
+      .then((result) => {
+        console.log(result);
+        //after successful call refetch data from db
+        this.fetchTasks();
+      }, (error) => {
+        console.log(error);
+      });
+    },
+    createTask(){
+      // clear validations
+      if(document.getElementById("floatingTaskName").classList.contains("is-invalid")){
+        document.getElementById("floatingTaskName").classList.remove("is-invalid");
+      }
+      if(document.getElementById("floatingTaskDescription").classList.contains("is-invalid")){
+        document.getElementById("floatingTaskDescription").classList.remove("is-invalid");
+      }
+
+      if(this.task.data.attributes.taskName !== "" && this.task.data.attributes.taskDescription !== ""){
+        //send task to api
+        this.postTask(this.task);
+        //reset model
+        this.resetTaskModel();
+        //hide modal
+        document.getElementById("modalCloseButton").click();
+      } else {
+        //if invalid set invalid
+        if(this.task.data.attributes.taskName === ""){
+          document.getElementById("floatingTaskName").classList.add("is-invalid");
+        }
+        if(this.task.data.attributes.taskDescription === ""){
+          document.getElementById("floatingTaskDescription").classList.add("is-invalid");
+        }
+      }
+    }
+  },
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
 </style>
